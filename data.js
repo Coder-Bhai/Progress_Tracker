61ac45e560ac0279655a8a9e673c652145b8c5b8
Coder Bhai <137440231+Coder-Bhai@users.noreply.github.com>
2024-08-14 21:46:47 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,103 +3,103 @@ const progressData = [
     // {
     //     date: '2024-08-26',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-25',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-24',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-23',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-22',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-21',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-20',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-19',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-18',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-17',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     // {
     //     date: '2024-08-16',
     //     tasks: [
-    //         { task: 'Task 1', status: 'Completed', notes: 'This is a note for task 1.' },
-    //         { task: 'Task 2', status: 'In Progress', notes: 'This is a note for task 2.' }
+    //         { task: 'Task 1', status: 'Completed'},
+    //         { task: 'Task 2', status: 'In Progress'}
     //     ]
     // },
 
     {
         date: '2024-08-15',
         tasks: [
-            { task: 'Python L1', status: 'Upcoming', notes: '-' },
-            { task: 'DBMS Week 1', status: 'Upcoming', notes: '-' },
-            { task: 'P&S', status: 'Upcoming', notes: '-' }
+            { task: 'Python L1', status: 'Upcoming'},
+            { task: 'DBMS Week 1', status: 'Upcoming'},
+            { task: 'P&S', status: 'Upcoming'}
         ]
     },
     {
         date: '2024-08-14',
         tasks: [
-            { task: 'Making Weekly Plan', status: 'In Progress', notes: '-' }
+            { task: 'Making Weekly Plan', status: 'In Progress'}
         ]
     }
 ];
